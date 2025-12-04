3d3eb4dae425019c82a4d61d34aa0919bb8eaf62
ptsfdtz <pitousanfadetuzi@gmail.com>
2025-12-04 17:17:55 +0800

    fix: github pages router

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,50 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
-import Main from '../pages/main';
-import Home from '../pages/home';
-import Mall from '../pages/mall';
-import User from '../pages/user';
-import PageOne from '../pages/other/pageOne';
-import PageTwo from '../pages/other/pageTwo';
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Main from "../pages/main";
+import Home from "../pages/home";
+import Mall from "../pages/mall";
+import User from "../pages/user";
+import PageOne from "../pages/other/pageOne";
+import PageTwo from "../pages/other/pageTwo";
 const routes = [
-    {
-        path: '/',
-        Component: Main,
+  {
+    path: "/",
+    Component: Main,
+    children: [
+      //重定向
+      {
+        path: "/",
+        element: <Navigate to="/home" replace />,
+      },
+      {
+        path: "home",
+        Component: Home,
+      },
+      {
+        path: "mall",
+        Component: Mall,
+      },
+      {
+        path: "user",
+        Component: User,
+      },
+      {
+        path: "other",
         children: [
-            //重定向
-            {
-                path: '/',
-                element: <Navigate to="/home" replace />
-            },
-            {
-                path: 'home',
-                Component: Home
-            },
-            {
-                path: 'mall',
-                Component: Mall
-            },
-            {
-                path: 'user',
-                Component: User
-            },
-            {
-                path: "other",
-                children: [
-                    {
-                        path: "PageOne",
-                        Component: PageOne
-                    },
-                    {
-                        path: "PageTwo",
-                        Component: PageTwo
-                    }
-                ]
-            }
-        ]
-    }
+          {
+            path: "PageOne",
+            Component: PageOne,
+          },
+          {
+            path: "PageTwo",
+            Component: PageTwo,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
-export default createBrowserRouter(routes);
+const basename =
+  process.env.NODE_ENV === "production" ? "/react-management" : undefined;
+
+export default createBrowserRouter(routes, { basename });
