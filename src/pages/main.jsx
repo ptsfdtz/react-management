96b31f3584461f4d8ceca9d1959b7e397e799735
ptsfdtz <pitousanfadetuzi@gmail.com>
2025-12-04 16:51:50 +0800

    fix: spell error in tab

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { Layout, theme } from 'antd';
-import CommonAside from '../components/commonAside';
-import CommonHeader from '../components/commonHeader';
-import { useSelector } from 'react-redux';
-import CommonTag from '../components/commonTag';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Layout, theme } from "antd";
+import CommonAside from "../components/commonAside";
+import CommonHeader from "../components/commonHeader";
+import { useSelector } from "react-redux";
+import CommonTag from "../components/commonTag";
 
 const { Content } = Layout;
 
 const Main = () => {
-    // const [collapsed, setCollapsed] = useState(false);
-    const {
-        token: { colorBgContainer, borderRadiusLG },
-    } = theme.useToken();
-    // 获取展开收起的状态   
-    const Collapsed = useSelector(state => state.tap.isCollapse)
-    return (
-        <Layout className='main-container'>
-            <CommonAside collapsed={Collapsed} />
-            <Layout>
-                <CommonHeader collapsed={Collapsed} />
-                <CommonTag />
-                <Content
-                    style={{
-                        margin: '24px 16px',
-                        padding: 24,
-                        minHeight: 280,
-                        background: colorBgContainer,
-                        borderRadius: borderRadiusLG,
-                    }}
-                >
-                    <Outlet></Outlet>
-                </Content>
-            </Layout>
-        </Layout>
-    );
-}
+  // const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  // 获取展开收起的状态
+  const Collapsed = useSelector((state) => state.tab.isCollapse);
+  return (
+    <Layout className="main-container">
+      <CommonAside collapsed={Collapsed} />
+      <Layout>
+        <CommonHeader collapsed={Collapsed} />
+        <CommonTag />
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Outlet></Outlet>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
 export default Main;
--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import TapReducer from "./reducers/tap";
 
 const store = configureStore({
-    reducer: {
-        tap: TapReducer
-    }
+  reducer: {
+    tab: TapReducer,
+  },
 });
 
 export default store;
